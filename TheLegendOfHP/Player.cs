e6215bccb3d191e9215d1471a7d12a63887dfaf0
nnn <you@example.com>
2022-11-17 18:44:52 +0100

    commit3_player

--- TheLegendOfHP/Player.cs
+++ TheLegendOfHP/Player.cs
@@ -8,9 +8,20 @@ namespace TheLegendOfHP
 {
     public class Player : Entity
     {
+        public Player(int lvl) : base(lvl)
+        {
+            level = lvl;
+            maxHealthPoints = 15 * level;
+            atack = 10 * level;
+            defence = 5 * level;
+            velocity = 5 * level;
+            healthPoints = MaxHealthPoints;
+            isAlive = true;
+
+        }
 
 
 
-        
+
     }
 }
